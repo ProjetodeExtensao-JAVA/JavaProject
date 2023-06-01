82eb27debe8f86b06528f9600a79d19a2a115cdf
Antonio Victor <vgoncalvescarvalho@outlook.com>
2023-06-01 13:10:50 -0300

    programa compilando e app fechando

--- APP Mobile - Android Studio/app/src/main/java/com/example/mobileapp/Actvities/telas/tela_login.java
+++ APP Mobile - Android Studio/app/src/main/java/com/example/mobileapp/Actvities/telas/tela_login.java
@@ -1,13 +1,13 @@
 package com.example.mobileapp.Actvities.telas;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobileapp.R;
 
@@ -22,11 +22,25 @@ public class tela_login extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent( tela_login.this, MainActivity.class));
+                TextView tLogin = (TextView) findViewById(R.id.inputLogin);
+                TextView tSenha = (TextView) findViewById(R.id.inputSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+                Intent x = new Intent(tela_login.this, usuario_logado.class);
+                Intent y = new Intent(tela_login.this, MainActivity.class);
+                if(login.equals("Toin") && senha.equals("123")){
+                    alert("Login realizado com sucesso!");
+                    startActivity(x);
+                }if(login.equals("admin") && senha.equals("admin")){
+                    alert("Login realizado com sucesso!");
+                    startActivity(y);
+                } else{
+                    alert("Login incorreto!");
+                }
             }
         });
     }
-    private void alert(String s){Toast.makeText(this, s, Toast.LENGTH_LONG).show();}
+    private void alert(String s){
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
 }
-
-
