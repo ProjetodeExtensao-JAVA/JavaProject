4f1aa172561d7d457eacfdf2073b93925fafde96
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2023-06-12 12:13:13 -0300

    fotos aparecendo, ainda não são enviadas ao banco, mas já existe a tabela

--- APP Mobile - Android Studio/app/src/main/java/com/example/mobileapp/Actvities/telas/usuario_logado.java
+++ APP Mobile - Android Studio/app/src/main/java/com/example/mobileapp/Actvities/telas/usuario_logado.java
@@ -1,3 +1,5 @@
+
+
 package com.example.mobileapp.Actvities.telas;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -13,9 +15,12 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,186 +33,213 @@ import com.example.mobileapp.Actvities.dao.DaoCliente;
 import com.example.mobileapp.Actvities.model.ModelCliente;
 import com.example.mobileapp.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class usuario_logado extends AppCompatActivity {
+    public class usuario_logado extends AppCompatActivity {
 
-    private EditText campoKm;
-    private Button botaoEnviarKm;
-    private Button btnCarregarFoto;
-    private DaoCliente daoCliente;
-    private ImageView receberFoto;
-    private List<Uri> listaUrisSelecionadas = new ArrayList<>();
-    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+        private EditText campoKm;
+        private Button botaoEnviarKm;
+        private Button btnCarregarFoto;
+        private DaoCliente daoCliente;
+        private List<Uri> listaUrisSelecionadas = new ArrayList<>();
+        private ActivityResultLauncher<Intent> someActivityResultLauncher;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_usuario_logado);
-        inicializarComponentes();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_usuario_logado);
+            inicializarComponentes();
 
-        daoCliente = new DaoCliente(this);
-        String cpfUsuario = getIntent().getStringExtra("cpf");
+            daoCliente = new DaoCliente(this);
+            String cpfUsuario = getIntent().getStringExtra("cpf");
 
-        // Chama o método para obter o modelo e a placa do usuário logado
-        obterModeloPlaca(cpfUsuario);
+            // Chama o método para obter o modelo e a placa do usuário logado
+            obterModeloPlaca(cpfUsuario);
 
-        botaoEnviarKm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateKm(v);
-            }
-        });
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
-            // Lidar com a situação em que a permissão não está concedida
-            // Solicitar permissão ao usuário ou realizar alguma ação alternativa
-            // ...
-            return;
-        }
-
-        receberFoto = findViewById(R.id.receberFoto);
-        btnCarregarFoto = findViewById(R.id.btnCarregarFoto);
-
-        btnCarregarFoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    String[] permissao = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                    requestPermissions(permissao, 1001);
-                } else {
-                    escolherImagens();
+            botaoEnviarKm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateKm(v);
                 }
-            }
-        });
+            });
 
-        someActivityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = result.getData();
-                        if (data != null) {
-                            if (data.getClipData() != null) {
-                                int count = data.getClipData().getItemCount();
-                                for (int i = 0; i < count; i++) {
-                                    Uri imageUri = data.getClipData().getItemAt(i).getUri();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+                // Lidar com a situação em que a permissão não está concedida
+                // Solicitar permissão ao usuário ou realizar alguma ação alternativa
+                // ...
+                return;
+            }
+
+            btnCarregarFoto = findViewById(R.id.btnCarregarFoto);
+
+            btnCarregarFoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        String[] permissao = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissao, 1001);
+                    } else {
+                        escolherImagens();
+                    }
+                }
+            });
+
+            someActivityResultLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            if (data != null) {
+                                if (data.getClipData() != null) {
+                                    int count = Math.min(data.getClipData().getItemCount(), 4); // Limite para 4 fotos
+                                    for (int i = 0; i < count; i++) {
+                                        Uri imageUri = data.getClipData().getItemAt(i).getUri();
+                                        listaUrisSelecionadas.add(imageUri);
+                                    }
+                                    exibirImagensSelecionadas();
+                                } else if (data.getData() != null) {
+                                    Uri imageUri = data.getData();
                                     listaUrisSelecionadas.add(imageUri);
+                                    exibirImagensSelecionadas();
                                 }
-                                exibirImagensSelecionadas();
-                            } else if (data.getData() != null) {
-                                Uri imageUri = data.getData();
-                                listaUrisSelecionadas.add(imageUri);
-                                exibirImagensSelecionadas();
                             }
                         }
                     }
-                }
-        );
-    }
-
-    public void inicializarComponentes() {
-        campoKm = findViewById(R.id.idQuilometragem);
-        botaoEnviarKm = findViewById(R.id.idEnviar);
-    }
-
-    private void obterModeloPlaca(String cpf) {
-        ModelCliente cliente = daoCliente.obterModeloPlaca(cpf);
-        if (cliente != null) {
-            // Exiba o modelo e a placa nos campos de texto correspondentes
-            TextView modeloCarro = findViewById(R.id.modeloCarro);
-            TextView placaCarro = findViewById(R.id.placaCarro);
-            modeloCarro.setText(cliente.getCliModelo());
-            placaCarro.setText(cliente.getCliPlaca());
-        } else {
-            // Não foi possível obter as informações do modelo e placa
-            Toast.makeText(this, "Erro ao obter informações do carro", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void updateKm(View view) {
-        String txtKmString = campoKm.getText().toString();
-        int txtkm = Integer.parseInt(txtKmString);
-
-        // Obtenha o CPF do usuário logado
-        String cpfUsuario = getIntent().getStringExtra("cpf");
-
-        ModelCliente cliente = new ModelCliente();
-        cliente.setCliCPF(cpfUsuario);  // Defina o CPF do usuário logado
-        cliente.setCliKm(txtkm);  // Defina a nova quilometragem
-
-        boolean resultado = daoCliente.updateKm(cliente);
-
-        if (resultado) {
-            Toast.makeText(this, "Quilometragem atualizada com sucesso", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Erro ao atualizar a quilometragem", Toast.LENGTH_SHORT).show();
+            );
         }
 
-        if (txtkm % 10000 == 0) {
-            exibirNotificacao("O carro atingiu 10.000Km", "Por favor levar na revisão!!!");
-        }
-    }
-
-    private void exibirNotificacao(String titulo, String conteudo) {
-        // Cria um canal de notificação
-        criarCanalNotificacao();
-
-        // Cria a notificação
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "seu_canal_id")
-                .setSmallIcon(R.drawable.logo_hot_wheels)
-                .setContentTitle(titulo)
-                .setContentText(conteudo)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
-            // A permissão VIBRATE não foi concedida pelo usuário
-            // Você pode solicitar a permissão aqui ou tratar o caso de permissão negada de outra forma
-            return;
+        public void inicializarComponentes() {
+            campoKm = findViewById(R.id.idQuilometragem);
+            botaoEnviarKm = findViewById(R.id.idEnviar);
         }
 
-        // Obtém o NotificationManagerCompat
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        // Exibe a notificação
-        int notificationId = 1; // ID único para a notificação
-        notificationManager.notify(notificationId, builder.build());
-    }
-
-    private void criarCanalNotificacao() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = "seu_canal_id";
-            CharSequence channelName = "Seu Canal";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private void escolherImagens() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        someActivityResultLauncher.launch(intent);
-    }
-
-    private void exibirImagensSelecionadas() {
-        if (!listaUrisSelecionadas.isEmpty()) {
-            LinearLayout linearLayout = findViewById(R.id.linearLayoutImagens); // ID do LinearLayout que irá conter as imagens
-
-            linearLayout.removeAllViews(); // Remove as visualizações anteriores se houver
-
-            for (Uri imageUri : listaUrisSelecionadas) {
-                ImageView imageView = new ImageView(this);
-                imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                imageView.setImageURI(imageUri);
-
-                linearLayout.addView(imageView); // Adiciona a ImageView ao LinearLayout
+        private void obterModeloPlaca(String cpf) {
+            ModelCliente cliente = daoCliente.obterModeloPlaca(cpf);
+            if (cliente != null) {
+                // Exiba o modelo e a placa nos campos de texto correspondentes
+                TextView modeloCarro = findViewById(R.id.modeloCarro);
+                TextView placaCarro = findViewById(R.id.placaCarro);
+                modeloCarro.setText(cliente.getCliModelo());
+                placaCarro.setText(cliente.getCliPlaca());
+            } else {
+                // Não foi possível obter as informações do modelo e placa
+                Toast.makeText(this, "Erro ao obter informações do carro", Toast.LENGTH_SHORT).show();
             }
         }
-    }
 
+        public void updateKm(View view) {
+            String txtKmString = campoKm.getText().toString();
+            int txtkm = Integer.parseInt(txtKmString);
+
+            // Obtenha o CPF do usuário logado
+            String cpfUsuario = getIntent().getStringExtra("cpf");
+
+            ModelCliente cliente = new ModelCliente();
+            cliente.setCliCPF(cpfUsuario);  // Defina o CPF do usuário logado
+            cliente.setCliKm(txtkm);  // Defina a nova quilometragem
+
+            boolean resultado = daoCliente.updateKm(cliente);
+
+            if (resultado) {
+                Toast.makeText(this, "Quilometragem atualizada com sucesso", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Erro ao atualizar a quilometragem", Toast.LENGTH_SHORT).show();
+            }
+
+            if (txtkm % 10000 == 0) {
+                exibirNotificacao("O carro atingiu 10.000Km", "Por favor levar na revisão!!!");
+            }
+        }
+
+        private void exibirNotificacao(String titulo, String conteudo) {
+            // Cria um canal de notificação
+            criarCanalNotificacao();
+
+            // Cria a notificação
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "seu_canal_id")
+                    .setSmallIcon(R.drawable.logo_hot_wheels)
+                    .setContentTitle(titulo)
+                    .setContentText(conteudo)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+                // A permissão VIBRATE não foi concedida pelo usuário
+                // Você pode solicitar a permissão aqui ou tratar o caso de permissão negada de outra forma
+                return;
+            }
+
+            // Obtém o NotificationManagerCompat
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            // Exibe a notificação
+            int notificationId = 1; // ID único para a notificação
+            notificationManager.notify(notificationId, builder.build());
+        }
+
+        private void criarCanalNotificacao() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                String channelId = "seu_canal_id";
+                CharSequence channelName = "Seu Canal";
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+                NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        private void escolherImagens() {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            someActivityResultLauncher.launch(intent);
+        }
+
+        private void exibirImagensSelecionadas() {
+            if (!listaUrisSelecionadas.isEmpty()) {
+                LinearLayout linearLayoutTop = findViewById(R.id.linearLayoutImagensTop); // ID do LinearLayout para as imagens superiores
+                LinearLayout linearLayoutBottom = findViewById(R.id.linearLayoutImagensBottom); // ID do LinearLayout para as imagens inferiores
+
+                linearLayoutTop.removeAllViews(); // Remove as visualizações anteriores se houver
+                linearLayoutBottom.removeAllViews(); // Remove as visualizações anteriores se houver
+
+                int maxImages = Math.min(listaUrisSelecionadas.size(), 4);  // Define o número máximo de imagens a serem exibidas como 4
+
+                for (int i = 0; i < maxImages; i++) {
+                    Uri imageUri = listaUrisSelecionadas.get(i);
+
+                    try {
+                        Bitmap bitmap = getResizedBitmap(MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri), 500, 500); // Redimensiona a imagem para 300x300 pixels
+                        ImageView imageView = new ImageView(this);
+                        imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                        imageView.setImageBitmap(bitmap);
+
+                        if (i < 2) {
+                            linearLayoutTop.addView(imageView); // Adiciona a ImageView ao layout das imagens superiores
+                        } else {
+                            linearLayoutBottom.addView(imageView); // Adiciona a ImageView ao layout das imagens inferiores
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+        private Bitmap getResizedBitmap(Bitmap bitmap, int newWidth, int newHeight) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            float scaleWidth = ((float) newWidth) / width;
+            float scaleHeight = ((float) newHeight) / height;
+            // Cria uma matriz para manipulação da imagem
+            Matrix matrix = new Matrix();
+            // Define o fator de escala para a matriz
+            matrix.postScale(scaleWidth, scaleHeight);
+            // Redimensiona a imagem com a matriz fornecida
+            return Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, false);
+        }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
